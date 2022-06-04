c04306f5c8b8e32d7478c2d17f8d86a434cf5c72
rafaelkenedy <k.rafaelalves@gmail.com>
2022-06-04 02:26:29 -0300

    created account class

---
+++ src/main/kotlin/dio/diobank/Conta.kt
@@ -0,0 +1,20 @@
+package dio.diobank
+
+import java.math.BigDecimal
+
+class Conta (
+
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+    ){
+
+    fun depositaValor(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+
+}
